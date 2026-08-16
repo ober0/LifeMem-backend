d8ab4aeca8240f5fb66575bb6fb2ef8cd7213fde
Ruslan <onishruslan@yandex.ru>
2026-08-16 21:10:47 +0300

    feat: scalar

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 import cookieParser from 'cookie-parser';
 import basicAuth from 'express-basic-auth';
 import { AppModule } from './modules/app/app.module';
@@ -56,21 +57,42 @@ async function bootstrap() {
     const config = new DocumentBuilder()
         .setTitle(`Api документация`)
         .setVersion('0.0.1')
-        .addGlobalParameters({
-            name: 'x-client-type',
-            in: 'header',
-            required: false,
-            description: 'Тип клиента',
-            schema: {
-                type: 'string',
-                enum: ['web', 'mobile'],
-                default: 'web'
+        .addBearerAuth()
+        .addGlobalParameters(
+            {
+                name: 'x-client-type',
+                in: 'header',
+                required: false,
+                description: 'Тип клиента',
+                schema: {
+                    type: 'string',
+                    enum: ['web', 'mobile'],
+                    default: 'web'
+                }
+            },
+            {
+                name: 'accept-language',
+                in: 'header',
+                required: false,
+                description: 'Язык',
+                schema: {
+                    type: 'string',
+                    enum: ['ru', 'en'],
+                    default: 'ru'
+                }
             }
-        })
+        )
         .build();
 
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('docs', app, document);
+
+    app.use(
+        '/docs',
+        apiReference({
+            content: document,
+            pageTitle: 'LifeMem API'
+        })
+    );
 
     await app.listen(port);
 }
