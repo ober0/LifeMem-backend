4e054ecfddd4f5969550e734a6cd7273ba707521
Ruslan <onishruslan@yandex.ru>
2026-08-20 23:17:45 +0300

    fix: lint

--- src/cron-tasks/cron.module.ts
+++ src/cron-tasks/cron.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 
 import { AuthRepository } from '../api/auth/repo/auth.repository';
 import { PrismaModule } from '../api/prisma/prisma.module';
-import { envConfigs, validateEnv } from '../common/config/env';
 import { CronWorker } from './cron.worker';
 import { DeleteExpiresConfirmCodesJob } from './tasks/delete-expires-confirm-codes';
 import { DeleteExpiresJwtJob } from './tasks/delete-expires-jwt';
