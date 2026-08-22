c2fdc68a9f74321803ca29dc969d7ad66177cd3d
Ruslan <onishruslan@yandex.ru>
2026-08-22 12:14:32 +0300

    разрешил google auth

--- src/api/auth/controllers/auth-google.controller.ts
+++ src/api/auth/controllers/auth-google.controller.ts
@@ -6,6 +6,7 @@ import type { Actor } from '../../../common/classes/actor';
 import { CurrentActor } from '../../../common/decorators/current-actor.decorator';
 import { ThrottleByIp, ThrottleByUser } from '../../../common/decorators/throttle-by-user.decorator';
 import { JwtAuthGuardHttp } from '../../../common/guards/auth.guard';
+import { apiError } from '../../../common/helpers/errors';
 import { ApiErrorResponses } from '../../../common/swagger/api-error-responses';
 import { translations } from '../../../common/translation/text-translations';
 import { GoogleAuthDto, GoogleLinkDto } from '../dto/google-auth.dto';
@@ -26,52 +27,46 @@ export class AuthGoogleController {
     @Post('login')
     @ApiOkResponse({ type: LoginResponseDto })
     async login(
-        @Body() _dto: GoogleAuthDto,
+        @Body() dto: GoogleAuthDto,
         @Req() request: express.Request,
-        @Res({ passthrough: true }) _response: express.Response
+        @Res({ passthrough: true }) response: express.Response
     ) {
-        const lang = request.actor?.requestLang;
-        return {
-            alert: true,
-            message: translations.byTextKey({ key: 'common.inDevelopment', lang })
+        const device = request.actor.device;
+
+        if (!device) {
+            throw apiError.internal('auth.device_context_missing');
+        }
+
+        const ip = device.ip;
+        const data = await this.authGoogleService.login(dto, ip);
+
+        response.cookie('refreshToken', data.refreshToken, {
+            secure: true,
+            sameSite: 'none',
+            httpOnly: true,
+            maxAge: 1000 * 3600 * 24 * 7
+        });
+
+        response.cookie('accessToken', data.accessToken, {
+            secure: true,
+            sameSite: 'none',
+            httpOnly: true,
+            maxAge: 1000 * 3600
+        });
+
+        let res: LoginResponseDto = {
+            user: data.user
         };
 
-        // const device = request.actor.device;
-        //
-        // if (!device) {
-        //     throw apiError.internal('auth.device_context_missing');
-        // }
-        //
-        // const ip = device.ip;
-        // const data = await this.authGoogleService.login(dto, ip);
-        //
-        // response.cookie('refreshToken', data.refreshToken, {
-        //     secure: true,
-        //     sameSite: 'none',
-        //     httpOnly: true,
-        //     maxAge: 1000 * 3600 * 24 * 7
-        // });
-        //
-        // response.cookie('accessToken', data.accessToken, {
-        //     secure: true,
-        //     sameSite: 'none',
-        //     httpOnly: true,
-        //     maxAge: 1000 * 3600
-        // });
-        //
-        // let res: LoginResponseDto = {
-        //     user: data.user
-        // };
-        //
-        // if (device.type === 'mobile') {
-        //     res = {
-        //         ...res,
-        //         accessToken: data.accessToken,
-        //         refreshToken: data.refreshToken
-        //     };
-        // }
-        //
-        // return res;
+        if (device.type === 'mobile') {
+            res = {
+                ...res,
+                accessToken: data.accessToken,
+                refreshToken: data.refreshToken
+            };
+        }
+
+        return res;
     }
 
     @ApiOperation({ summary: 'Привязка Google к существующему аккаунту' })
@@ -81,12 +76,8 @@ export class AuthGoogleController {
     @ApiBearerAuth()
     @ApiOkResponse()
     @ApiErrorResponses(401)
-    async link(@Body() _dto: GoogleLinkDto, @CurrentActor() _actor: Actor) {
-        return {
-            alert: true,
-            message: translations.byTextKey({ key: 'common.inDevelopment' })
-        };
-        // await this.authGoogleService.link(dto, actor);
+    async link(@Body() dto: GoogleLinkDto, @CurrentActor() actor: Actor) {
+        await this.authGoogleService.link(dto, actor);
     }
 
     @ApiOperation({ summary: 'Отвязать Google от аккаунта' })
@@ -96,11 +87,7 @@ export class AuthGoogleController {
     @ApiBearerAuth()
     @ApiOkResponse({ description: 'Google успешно отвязан' })
     @ApiErrorResponses(401)
-    async unlink(@CurrentActor() _actor: Actor) {
-        return {
-            alert: true,
-            message: translations.byTextKey({ key: 'common.inDevelopment' })
-        };
-        // await this.authGoogleService.unlink(actor);
+    async unlink(@CurrentActor() actor: Actor) {
+        await this.authGoogleService.unlink(actor);
     }
 }
